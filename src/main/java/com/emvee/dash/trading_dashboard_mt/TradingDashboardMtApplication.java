dc4ca395314aca126b127a5bd24c9f819d44836c
emvee <vercammen.maarten.2000@gmail.com>
2025-03-08 09:12:34 +0100

    DASH-32: init project

---
+++ src/main/java/com/emvee/dash/trading_dashboard_mt/TradingDashboardMtApplication.java
@@ -0,0 +1,13 @@
+package com.emvee.dash.trading_dashboard_mt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingDashboardMtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingDashboardMtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emvee/dash/trading_dashboard_mt/TradingDashboardMtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emvee.dash.trading_dashboard_mt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingDashboardMtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
